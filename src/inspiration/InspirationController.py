f0c5bb53b74364daf56ed3673a9b9bfdc16ecc5e
Syahrizal Khairan <13523063@std.stei.itb.ac.id>
2024-12-14 11:18:45 +0700

    feat: inspiration controller methods

--- src/inspiration/InspirationController.py
+++ src/inspiration/InspirationController.py
@@ -9,7 +9,7 @@ class InspirationController:
         self.db = DBConnection()
         self.getAllProjects()
 
-    def getAllProjects(self):
+    def getAllInspirations(self):
         inspirations = []
         result = self.db.getAllProjects()
         for row in result:
@@ -22,6 +22,36 @@ class InspirationController:
             tags = self.db.getTagsById(inspiration.getInspirationId())
             inspiration.setTags(tags)
             inspirations.append(inspiration)
+        
+        return inspirations
+
+    def getInspiration(self, inspiration_id: int):
+        inspiration = Inspiration()
+        result = self.db.getProject(inspiration_id)
+        for row in result:
+            inspiration.setInspirationId(row[0])
+            inspiration.setName(row[1])
+            inspiration.setCachedImagePath(row[2])
+            inspiration.setExternalLink(row[3])
+            inspiration.setDateUpdated(row[4])
+            tags = self.db.getTagsById(inspiration.getInspirationId())
+            inspiration.setTags(tags)
+        
+        return inspiration
+
+    def createInspiration(self):
+        inspiration_form = InspirationForm()
+        inspiration_id, name, cached_image_path, link, date_updated, tags = inspiration_form.inputInspiration()
+        self.db.createInspiration(inspiration_id, name, cached_image_path, link, date_updated, tags)
+    
+    def editInspiration(self):
+        inspiration_form = InspirationForm()
+        inspiration_id, name, cached_image_path, link, date_updated, tags = inspiration_form.inputInspiration()
+        self.db.editInspiration(inspiration_id, name, cached_image_path, link, date_updated, tags)
+
+    def deleteInspiration(self, inspiration_id: int):
+        self.db.deleteInspiration(inspiration_id)
+
 
 if __name__=='__main__':
     import customtkinter as ctk
